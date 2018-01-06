49552afbb3a0048ae16f6c905bb8b5d069977739
nel215 <otomo.yuhei@gmail.com>
2018-01-06 22:08:43 +0900

    Refactor model

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import os
 import numpy as np
 import gym
-import chainer
-import chainer.links as L
-import chainer.functions as F
 import random
 from PIL import Image
 from dask.distributed import Client
 from neuroevolution import MinioModelStore
+from neuroevolution.model import create_model
 
 
 def set_random_seed(seed):
@@ -23,41 +21,16 @@ def get_model_store():
     return model_store
 
 
-class DNN(chainer.Chain):
-    def __init__(self, n_action):
-        super(DNN, self).__init__()
-        with self.init_scope():
-            self.l1 = L.Linear(None, 5)
-            self.l2 = L.Linear(None, n_action)
-
-    def update(self):
-        for p in self.params():
-            p.data += np.random.randn(*p.shape) * 0.005
-
-    def __call__(self, x):
-        h1 = F.relu(self.l1(x))
-        y = self.l2(x)
-        return y
-
-
-def get_action(state, dnn):
-    state = np.array([state], dtype=np.float32)
-    with chainer.using_config('train', False):
-        act = F.argmax(dnn(state))
-
-    return int(act.data)
-
-
 def initialize_network(seed, store=False, name=None):
     set_random_seed(seed)
     env = gym.make('CartPole-v0')
     env.seed(0)
     n_action = env.action_space.n
-    dnn = DNN(n_action)
+    dnn = create_model(n_action)
     state = env.reset()
     total_reward = 0
     while True:
-        act = get_action(state, dnn)
+        act = dnn.get_action(state)
         state, reward, done, info = env.step(act)
         total_reward += reward
         if done:
@@ -75,7 +48,7 @@ def update_network(seed, store=False, name=None):
     env = gym.make('CartPole-v0')
     env.seed(0)
     n_action = env.action_space.n
-    dnn = DNN(n_action)
+    dnn = create_model(n_action)
     rank = random.randint(0, 2)
     if seed == 0:
         pre_model_name = 'top-0'
@@ -89,7 +62,7 @@ def update_network(seed, store=False, name=None):
     state = env.reset()
     total_reward = 0
     while True:
-        act = get_action(state, dnn)
+        act = dnn.get_action(state)
         state, reward, done, info = env.step(act)
         total_reward += reward
         if done:
---
+++ neuroevolution/model/__init__.py
@@ -0,0 +1,32 @@
+import numpy as np
+import chainer
+import chainer.links as L
+import chainer.functions as F
+
+
+class DNN(chainer.Chain):
+    def __init__(self, n_action):
+        super(DNN, self).__init__()
+        with self.init_scope():
+            self.l1 = L.Linear(None, 5)
+            self.l2 = L.Linear(None, n_action)
+
+    def update(self):
+        for p in self.params():
+            p.data += np.random.randn(*p.shape) * 0.005
+
+    def get_action(self, state):
+        state = np.array([state], dtype=np.float32)
+        with chainer.using_config('train', False):
+            act = F.argmax(self(state))
+
+        return int(act.data)
+
+    def __call__(self, x):
+        h1 = F.relu(self.l1(x))
+        y = self.l2(x)
+        return y
+
+
+def create_model(n_action):
+    return DNN(n_action)
